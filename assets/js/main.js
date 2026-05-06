32a6aa7f58ea3d69354a3412c260d1a954d36a98
DUKY-now <dukynow@gmail.com>
2026-05-06 16:48:30 +0200

    changement pop-up

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,3 +45,61 @@ document.querySelectorAll('.clickable').forEach(card => {
 function toggleCard(card) {
     card.classList.toggle("active");
 }
+
+let images = [];
+let current = 0;
+let interval;
+
+/* OPEN MODAL */
+function openModal(data) {
+    document.getElementById("modal").style.display = "flex";
+
+    // Injecter texte
+    document.getElementById("modal-title").textContent = data.title;
+    document.getElementById("modal-desc").textContent = data.desc;
+    document.getElementById("modal-role").textContent = data.role;
+    document.getElementById("modal-equip").textContent = data.equip;
+
+    // Images
+    images = data.images;
+    current = 0;
+
+    startSlider();
+}
+
+/* CLOSE */
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+    clearInterval(interval);
+}
+
+/* SHOW SLIDE */
+function showSlide(index) {
+    const img = document.getElementById("slider-image");
+    const count = document.getElementById("slider-count");
+
+    img.src = images[index];
+    count.textContent = (index + 1) + " / " + images.length;
+}
+
+/* NEXT */
+function nextSlide() {
+    current = (current + 1) % images.length;
+    showSlide(current);
+}
+
+/* PREV */
+function prevSlide() {
+    current = (current - 1 + images.length) % images.length;
+    showSlide(current);
+}
+
+/* AUTOPLAY */
+function startSlider() {
+    showSlide(current);
+
+    clearInterval(interval);
+    interval = setInterval(() => {
+        nextSlide();
+    }, 3000);
+}
